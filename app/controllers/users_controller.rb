e0577bfc07057a997a01f0a474105a85a3aa9a2e
jackcooper1245 <jackcooper1245@gmail.com>
2020-09-29 15:29:48 +0200

    Added Welcome controller and New User Form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.save
+            redirect_to root_path
+        else 
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def delete
+    end
+
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:user_name, :first_name, :last_name, :email, :password, :password_confirmation, :age)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     has_one :budget
     has_many :treats, through: :lists
     validates :user_name, presence: true
+    validates :user_name, uniqueness: true
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :email, presence: true
