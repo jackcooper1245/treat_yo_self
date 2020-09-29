0d91ed20c56afff32ac0f74fa89e6b713dd61006
jackcooper1245 <jackcooper1245@gmail.com>
2020-09-29 14:54:05 +0200

    finished writing tests for all models

--- spec/models/budget_spec.rb
+++ spec/models/budget_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Budget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user =   User.create(:user_name => "jackcooper1245", :first_name => "Jack", :last_name => "Cooper", :email => "jackcooper1245@gmail.com", :password => "password", :age => "25")
+    @list_1 = List.create(:name => "Weekend fun", :week => "1st week of August", :user => @user)
+    @list_2 = List.create(:name => "Week not-fun", :week => "2nd week of August", :user => @user)
+    @treat_1 = Treat.create(:name => "Beer", :cost => 3, :user => @user, :list => @list_1)
+    @treat_2 = Treat.create(:name => "Curry", :cost => 12, :user => @user, :list => @list_2)
+    @budget = Budget.create(:name => "Weekly Budget", :total => 50, :user => @user, :currency => "£")
+  end
+
+  it "should be valid with a name, a total and a currency" do
+    expect(@budget).to be_valid
+  end
+
+  it "should belong to a user" do
+    t = Budget.reflect_on_association(:user)
+    expect(t.macro).to eq(:belongs_to)
+  end
+
+  it "should have many lists, through a user" do
+    expect(@budget.lists.first).to eq(@list_1)
+    expect(@budget.lists.last).to eq(@list_2)
+  end
+
+  it "should have many treats, through many lists" do
+    expect(@budget.treats.first).to eq(@treat_1)
+    expect(@budget.treats.last).to eq(@treat_2)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,19 +2,17 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
-  let(:user) {
-    User.create(
-      :user_name => "jackcooper1245",
-      :first_name => "Jack",
-      :last_name => "Cooper",
-      :email => "jackcooper1245@gmail.com",
-      :password => "password",
-      :age => "25"
-    )
-  }
+  before do
+    @user =   User.create(:user_name => "jackcooper1245", :first_name => "Jack", :last_name => "Cooper", :email => "jackcooper1245@gmail.com", :password => "password", :age => "25")
+    @budget = Budget.create(:name => "Monthly Budget", :total => 100, :user => @user, :currency => "£")
+    @list_1 = List.create(:name => "Weekend fun", :week => "1st week of August", :user => @user)
+    @list_2 = List.create(:name => "Week not-fun", :week => "2nd week of August", :user => @user)
+    @treat_1 = Treat.create(:name => "Beer", :cost => 3, :user => @user, :list => @list_1)
+    @treat_2 = Treat.create(:name => "Curry", :cost => 12, :user => @user, :list => @list_2)
+  end
 
   it "is valid with a user_name, first_name, last_name, email, password, and age" do
-    expect(user).to be_valid
+    expect(@user).to be_valid
   end
 
   it "is not valid without a password" do
@@ -22,23 +20,17 @@ RSpec.describe User, type: :model do
   end
 
   it "has many lists" do
-    first_list = List.create(:user_id => user.id)
-    second_list = List.create(:user_id => user.id)
-    expect(user.lists.first).to eq(first_list)
-    expect(user.lists.last).to eq(second_list)
+    expect(@user.lists.first).to eq(@list_1)
+    expect(@user.lists.last).to eq(@list_2)
   end
 
-  it "has many budgets" do
-    first_budget = Budget.create(:user_id => user.id)
-    second_budget = Budget.create(:user_id => user.id)
-    expect(user.budgets.first).to eq(budget_list)
-    expect(user.budgets.last).to eq(budget_list)
+  it "has a budget" do
+    expect(@user.budget).to eq(@budget)
   end
 
   it "has many treats through lists" do
-    user.treats << [order_pizza, buy_clothes]
-    expect(user.treats.first).to eq(order_pizza)
-    expect(user.treats.last).to eq(buy_clothes)
+    expect(@user.treats.first).to eq(@treat_1)
+    expect(@user.treats.last).to eq(@treat_2)
   end
 
 end
